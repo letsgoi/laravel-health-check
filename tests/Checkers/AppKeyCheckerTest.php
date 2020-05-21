419138bd0136aff2550c570c2d4a6d20c684eb6d
Marco Bellido <marco.bellido@letsgoi.com>
2020-05-21 14:01:42 +0200

    feature: added app key checker

---
+++ tests/Checkers/AppKeyCheckerTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Letsgoi\HealthCheck\Tests;
+
+use Illuminate\Support\Facades\Config;
+use Letsgoi\HealthCheck\Checkers\AppKeyChecker;
+
+class AppKeyCheckerTest extends TestCase
+{
+    /** @test */
+    public function it_should_return_true_if_app_key_is_setted()
+    {
+        Config::set('app.key', 'ANY_KEY');
+
+        $appKeyChecker = new AppKeyChecker();
+
+        $this->assertTrue($appKeyChecker->check());
+    }
+
+    /** @test */
+    public function it_should_return_false_if_app_key_is_not_setted()
+    {
+        Config::set('app.key', null);
+
+        $appKeyChecker = new AppKeyChecker();
+
+        $this->assertFalse($appKeyChecker->check());
+    }
+}
