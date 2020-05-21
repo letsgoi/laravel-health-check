57e011e2bc8bf1bcb0e4957bd8af011b82855ffa
Marco Bellido <marco.bellido@letsgoi.com>
2020-05-21 16:03:39 +0200

    feature: added writable paths checker

---
+++ tests/Checkers/WritablePathsCheckerTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Letsgoi\HealthCheck\Tests;
+
+use Illuminate\Filesystem\Filesystem;
+use Letsgoi\HealthCheck\Checkers\WritablePathsChecker;
+use Mockery\MockInterface;
+
+class WritablePathsCheckerTest extends TestCase
+{
+    /** @test */
+    public function it_should_return_true_if_storage_and_cache_paths_are_writable()
+    {
+        $this->mock(Filesystem::class, static function(MockInterface $mock) {
+            $mock->shouldReceive('isWritable')
+                ->with(base_path('bootstrap/cache'))
+                ->once()
+                ->andReturn(true);
+
+            $mock->shouldReceive('isWritable')
+                ->with(base_path('storage'))
+                ->once()
+                ->andReturn(true);
+        });
+
+        $writablePathsChecker = new WritablePathsChecker();
+
+        $this->assertTrue($writablePathsChecker->check());
+    }
+
+    /** @test */
+    public function it_should_return_false_if_cache_path_is_not_writable()
+    {
+        $this->mock(Filesystem::class, static function(MockInterface $mock) {
+            $mock->shouldReceive('isWritable')
+                ->with(base_path('bootstrap/cache'))
+                ->once()
+                ->andReturn(false);
+        });
+
+        $writablePathsChecker = new WritablePathsChecker();
+
+        $this->assertFalse($writablePathsChecker->check());
+    }
+
+    /** @test */
+    public function it_should_return_false_if_storage_path_is_not_writable()
+    {
+        $this->mock(Filesystem::class, static function(MockInterface $mock) {
+            $mock->shouldReceive('isWritable')
+                ->with(base_path('bootstrap/cache'))
+                ->once()
+                ->andReturn(true);
+
+            $mock->shouldReceive('isWritable')
+                ->with(base_path('storage'))
+                ->once()
+                ->andReturn(false);
+        });
+
+        $writablePathsChecker = new WritablePathsChecker();
+
+        $this->assertFalse($writablePathsChecker->check());
+    }
+}
